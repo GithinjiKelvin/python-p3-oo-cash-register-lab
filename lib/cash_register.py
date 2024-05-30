4f4a4adb075a378153c19dc4e1c4437cb3544685
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-05-30 04:35:10 +0300

    complete lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    
+    total = 0
+    items = []
+
+    def __init__(self, discount = 0):
+        self.discount = discount
+
+    def add_item(self, title, price, quantity=1):
+        CashRegister.total += (price * quantity)
